fa15cade3d04e8b8ce3b13b0ec92a07b70ac8c81
Halazv2 <halaziani2003@gmail.com>
2021-11-16 17:03:12 +0100

    m

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 
 int main()
 {
-    int x,y=0;
-
-  while(y==0){
+    int tab[50],x=0,compteur=0;
+    do {
     printf("Enter the number x: ");
-    scanf("%d", &x);
-    if (x<0)
-        break;
-  }
-}
+    scanf("%d",&tab[x]);
+   x++;
+    }while(tab[x-1]>0);
+
+    for (int y=0;y<x-1;y++){
+        printf("entier positif:%d\n",tab[y]);
+        compteur++;
+
+        }
+        printf("\nVous avez entrer %d entiers positif", compteur);
+         return 0;
+    }

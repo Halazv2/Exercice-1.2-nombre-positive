45ed0e2047c97a09486499a6985ba2c2ad21d5a9
Halazv2 <halaziani2003@gmail.com>
2021-11-17 11:27:55 +0100

    edit

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+     void NP(int tab[], int x){
+        int compteur=0;
+        for (int y=0;y<x-1;y++){
+            printf("entier positif:%d\n",tab[y]);
+            compteur++;
+            }
+        printf("\nVous avez entrer %d entiers positif", compteur);
+
+        }
 int main()
 {
     int tab[50],x=0,compteur=0;
@@ -9,12 +19,6 @@ int main()
     scanf("%d",&tab[x]);
    x++;
     }while(tab[x-1]>0);
-
-    for (int y=0;y<x-1;y++){
-        printf("entier positif:%d\n",tab[y]);
-        compteur++;
-
-        }
-        printf("\nVous avez entrer %d entiers positif", compteur);
+    NP(tab,x);
          return 0;
     }
